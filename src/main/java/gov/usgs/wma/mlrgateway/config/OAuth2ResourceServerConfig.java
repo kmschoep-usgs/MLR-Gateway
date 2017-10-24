cc1f66845999223263f6d291a2079738e42789a1
Steinich, Dave R. <drsteini@usgs.gov>
2017-10-24 12:33:12 -0500

    Auth - line in sand - 10/24

--- src/main/java/gov/usgs/wma/mlrgateway/config/OAuth2ResourceServerConfig.java
+++ src/main/java/gov/usgs/wma/mlrgateway/config/OAuth2ResourceServerConfig.java
@@ -1,55 +1,31 @@
 package gov.usgs.wma.mlrgateway.config;
 
-import java.io.IOException;
-
-import org.apache.commons.io.IOUtils;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
-import org.springframework.core.io.ClassPathResource;
-import org.springframework.core.io.Resource;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
-import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
-import org.springframework.security.oauth2.provider.token.DefaultTokenServices;
-import org.springframework.security.oauth2.provider.token.TokenStore;
-import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
-import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 
-@Configuration
-@EnableResourceServer
+//@Configuration
+//@EnableResourceServer
 public class OAuth2ResourceServerConfig extends ResourceServerConfigurerAdapter {
-//    @Override
-//    public void configure(ResourceServerSecurityConfigurer config) {
-//        config.tokenServices(tokenServices());
-//        config.resourceId(null);
-//    }
-// 
-//    @Bean
-//    public TokenStore tokenStore() {
-//        return new JwtTokenStore(accessTokenConverter());
-//    }
-// 
-//    @Bean
-//    public JwtAccessTokenConverter accessTokenConverter() {
-//    	final JwtAccessTokenConverter converter = new JwtAccessTokenConverter();
-//        Resource resource = new ClassPathResource("public.txt");
-//        String publicKey = null;
-//        try {
-//            publicKey = IOUtils.toString(resource.getInputStream());
-//        } catch (final IOException e) {
-//            throw new RuntimeException(e);
-//        }
-//        converter.setVerifierKey(publicKey);
-//        return converter;
-//    }
-// 
-//    @Bean
-//    @Primary
-//    public DefaultTokenServices tokenServices() {
-//        DefaultTokenServices defaultTokenServices = new DefaultTokenServices();
-//        defaultTokenServices.setTokenStore(tokenStore());
-//        return defaultTokenServices;
-//    }
+
+	@Override
+	public void configure(HttpSecurity http) throws Exception {
+		http
+			.authorizeRequests()
+				.antMatchers("/swagger-ui.html", "/swagger-resources/**", "/webjars/**", "/v2/**").permitAll()
+				.antMatchers("/health/**", "/info/**").permitAll()
+				.anyRequest().fullyAuthenticated()
+			.and()
+				.logout().permitAll()
+			.and()
+				.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+			.and()
+				.cors()
+			.and()
+				.csrf().disable()
+		;
+	}
 
 }
--- src/main/java/gov/usgs/wma/mlrgateway/config/WebSecurityConfig.java
+++ src/main/java/gov/usgs/wma/mlrgateway/config/WebSecurityConfig.java
@@ -1,15 +1,22 @@
 package gov.usgs.wma.mlrgateway.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
 import org.springframework.cloud.security.oauth2.client.feign.OAuth2FeignRequestInterceptor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.TaskScheduler;
 import org.springframework.scheduling.concurrent.ConcurrentTaskScheduler;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.oauth2.client.OAuth2ClientContext;
 import org.springframework.security.oauth2.client.resource.OAuth2ProtectedResourceDetails;
+import org.springframework.security.oauth2.provider.authentication.OAuth2AuthenticationManager;
+import org.springframework.security.oauth2.provider.authentication.OAuth2AuthenticationProcessingFilter;
+import org.springframework.security.oauth2.provider.token.ResourceServerTokenServices;
+import org.springframework.security.web.authentication.preauth.AbstractPreAuthenticatedProcessingFilter;
 
 import feign.RequestInterceptor;
 
@@ -17,10 +24,17 @@ import feign.RequestInterceptor;
 @EnableOAuth2Sso
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
+	@Value("${security.oauth2.resource.id}")
+	private String resourceId;
+
+	@Autowired
+	private ResourceServerTokenServices tokenServices;
+
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http
 			.httpBasic().disable()
+			.anonymous().disable()
 			.cors().and()
 			.authorizeRequests()
 				.antMatchers("/swagger-resources/**", "/webjars/**", "/v2/**").permitAll()
@@ -30,6 +44,8 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 				.logout().permitAll()
 			.and()
 				.csrf().disable()
+			.addFilterAfter(oAuth2AuthenticationProcessingFilter(), AbstractPreAuthenticatedProcessingFilter.class);
+
 		;
 	}
 
@@ -38,10 +54,28 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 		return new OAuth2FeignRequestInterceptor(oauth2ClientContext, resource);
 	}
 
-
 	@Bean
 	public TaskScheduler taskScheduler() {
-		return new ConcurrentTaskScheduler(); //single threaded by default
+		return new ConcurrentTaskScheduler();
+	}
+
+	private OAuth2AuthenticationProcessingFilter oAuth2AuthenticationProcessingFilter() {
+		OAuth2AuthenticationProcessingFilter oAuth2AuthenticationProcessingFilter = new OAuth2AuthenticationProcessingFilter();
+		oAuth2AuthenticationProcessingFilter.setAuthenticationManager(oauthAuthenticationManager());
+		oAuth2AuthenticationProcessingFilter.setStateless(false);
+
+		return oAuth2AuthenticationProcessingFilter;
+	}
+
+
+	private AuthenticationManager oauthAuthenticationManager() {
+		OAuth2AuthenticationManager oauthAuthenticationManager = new OAuth2AuthenticationManager();
+
+		oauthAuthenticationManager.setResourceId(resourceId);
+		oauthAuthenticationManager.setTokenServices(tokenServices);
+		oauthAuthenticationManager.setClientDetailsService(null);
+
+		return oauthAuthenticationManager;
 	}
 
 }
