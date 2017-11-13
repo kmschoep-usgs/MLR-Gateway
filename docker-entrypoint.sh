7dce4332fd0bf7c54df149b2c6be60c0ad8c5f73
Kathryn Schoephoester <kmschoep@usgs.gov>
2017-11-13 08:55:32 -0600

    update docker-endpoint to enable ssl connections

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -13,11 +13,7 @@ elif [ $oauthClientSecret_file ]; then
     OAUTH_CLIENT_SECRET_VAL=`cat $oauthClientSecret_file`
 fi
 
-if [ $sslStorePassword ]; then
-    keystorePassword=$sslStorePassword
-elif [ $sslStorePassword_file ]; then 
-    keystorePassword=`cat $sslStorePassword_file`
-fi
+keystorePassword=`cat $keystore_password_path`
 
 openssl pkcs12 -export -in $token_cert_path -inkey $token_key_path -name $keystoreOAuthKey -out oauth.p12 -password pass:$keystorePassword
 openssl pkcs12 -export -in $ssl_cert_path -inkey $ssl_key_path -name tomcat -out tomcat.p12
@@ -25,8 +21,9 @@ openssl pkcs12 -export -in $ssl_cert_path -inkey $ssl_key_path -name tomcat -out
 
 keytool -importcert -file $water_auth_cert_file -keystore ssl_trust_store.jks -storepass $keystorePassword -alias auth.nwis.usgs.gov -noprompt
 keytool -v -importkeystore -deststorepass $keystorePassword -destkeystore ssl_trust_store.jks -deststoretype JKS -srckeystore oauth.p12 -srcstorepass $keystorePassword -srcstoretype PKCS12 --noprompt
-keytool -v -importkeystore -deststorepass $keystorePassword -destkeystore ssl_trust_store.jks -srckeystore tomcat.p12 -srcstoretype PKCS12
+keytool -v -importkeystore -deststorepass $keystorePassword -destkeystore ssl_trust_store.jks -deststoretype JKS -srckeystore tomcat.p12 -srcstorepass $keystorePassword -srcstoretype PKCS12 -noprompt
 
+keytool -list -keystore $keystoreLocation -storepass $keystorePassword
 java -Djava.security.egd=file:/dev/./urandom -DdbPassword=$MYSQL_PASSWORD_VAL -DoauthClientSecret=$OAUTH_CLIENT_SECRET_VAL -Djavax.net.ssl.trustStore=ssl_trust_store.jks -Djavax.net.ssl.trustStorePassword=$keystorePassword -jar app.jar
 
 exec $?
