d5bc342c73f1d1425de41bb81d77d01a5191eb20
zmoore-usgs <zmoore@usgs.gov>
2017-11-16 12:00:42 -0600

    MLR-252: Removed token fetching functionality from the UI.

--- src/main/webapp/resources/ui.js
+++ src/main/webapp/resources/ui.js
@@ -57,36 +57,6 @@ function postDdot(url, success, error) {
 	}
 }
 
-function fetchToken() {
-	var url = "util/token";
-	$('#token-response').show();
-	$('#token-text').hide();
-	$("#fetch-token").hide();
-	$('#token-spinner').addClass('spinner');
-	
-	//Perform Upload
-	$.ajax({
-		url: url,
-		type: 'GET',
-		success: function(data) {
-			$('#token-text').show();
-			$('#token-text').text(data);
-			$('#token-spinner').removeClass('spinner');
-		},
-		error: function(response) {
-			if(response.status > 0){
-				$('#token-text').text(JSON.stringify(response));
-			} else {
-				$('#token-text').text("Connection Error with Gateway Service. If this issue persists please contact the support team.");
-			}
-			$('#token-text').show();
-			$('#token-text').css("color: red");
-			$('#token-spinner').removeClass('spinner');
-			$("#fetch-token").show();
-		}
-	});
-}
-
 function validateDdot() {
 	postDdot("workflows/ddots/validate", generalDdotSuccess, generalDdotError)
 }
