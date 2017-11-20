6713b75c854ca82e53408e3da0ff7fbcf9d08570
Steinich, Dave R. <drsteini@usgs.gov>
2017-11-20 12:11:56 -0600

    initial tests

--- src/test/java/gov/usgs/wma/mlrgateway/controller/rt/ControllerRtTest.java
+++ src/test/java/gov/usgs/wma/mlrgateway/controller/rt/ControllerRtTest.java
@@ -2,25 +2,37 @@ package gov.usgs.wma.mlrgateway.controller.rt;
 
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.embedded.LocalServerPort;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
-import org.springframework.security.authentication.TestingAuthenticationToken;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.oauth2.common.OAuth2AccessToken;
-import org.springframework.security.oauth2.provider.OAuth2Authentication;
-import org.springframework.security.oauth2.provider.OAuth2Request;
 import org.springframework.security.oauth2.provider.token.AuthorizationServerTokenServices;
 import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+
 import gov.usgs.wma.mlrgateway.Application;
+import gov.usgs.wma.mlrgateway.config.MethodSecurityConfig;
+import gov.usgs.wma.mlrgateway.config.MultipartSupportConfig;
 import gov.usgs.wma.mlrgateway.config.WebSecurityConfig;
 
-//@RunWith(SpringRunner.class)
-//@SpringBootTest(classes={Application.class, WebSecurityConfig.class}, webEnvironment=SpringBootTest.WebEnvironment.RANDOM_PORT)
+@RunWith(SpringRunner.class)
+@SpringBootTest(
+		classes={Application.class,
+				WebSecurityConfig.class,
+				MultipartSupportConfig.class,
+				MethodSecurityConfig.class},
+		webEnvironment=SpringBootTest.WebEnvironment.RANDOM_PORT,
+		properties={"maintenanceRoles=ROLE_DBA_55",
+				"security.oauth2.resource.jwt.keyValue=secret",
+				"authPublicKeyUrl=",
+				"security.require-ssl=false",
+				"server.ssl.enabled=false"}
+	)
 public abstract class ControllerRtTest {
 
 	@Autowired
@@ -34,23 +46,44 @@ public abstract class ControllerRtTest {
 	@Autowired
 	JwtAccessTokenConverter jwtAccessTokenConverter;
 
-	public String addBearerToken(final String username, String... authorities) {
-		OAuth2Request oauth2Request = new OAuth2Request(null, "testClient", null, true, null, null, null, null, null);
-		Authentication userauth = new TestingAuthenticationToken(username, null, authorities);
-		OAuth2Authentication oauth2auth = new OAuth2Authentication(oauth2Request, userauth);
-		OAuth2AccessToken token = tokenservice.createAccessToken(oauth2auth);
-		return jwtAccessTokenConverter.enhance(token, oauth2auth).getValue();
+	@Value("${security.oauth2.resource.jwt.keyValue}")
+	private String secret;
+
+	public static final String KNOWN_USER = "knownusr";
+
+	public static String createToken(String username, String email, String ... roles) throws Exception {
+		String jwt = JWT.create()
+				.withClaim("user_name", username)
+				.withArrayClaim("authorities", roles)
+				.withClaim("email", email)
+				.sign(Algorithm.HMAC256("secret"))
+				;
+		return jwt;
 	}
 
-	public HttpHeaders getNoAuthHeaders() {
+	public static HttpHeaders getAuthorizedHeaders(MediaType mediaType) {
+		return getHeaders(mediaType, KNOWN_USER, "known@usgs.gov", "ROLE_DBA_55");
+	}
+
+	public static HttpHeaders getUnauthorizedHeaders(MediaType mediaType) {
+		return getHeaders(mediaType, KNOWN_USER, "known@usgs.gov", "ROLE_UNKNOWN");
+	}
+
+	public static HttpHeaders getHeaders(MediaType mediaType, String username, String email, String ... roles) {
+		HttpHeaders headers = getHeaders(mediaType);
+		try {
+			headers.add(HttpHeaders.AUTHORIZATION, "Bearer " + createToken(username, email, roles));
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return headers;
+	}
+
+	public static HttpHeaders getHeaders(MediaType mediaType) {
 		HttpHeaders headers = new HttpHeaders();
-		headers.setContentType(MediaType.MULTIPART_FORM_DATA);
+		headers.setContentType(mediaType);
 		return headers;
 	}
 
-	public HttpHeaders getAuthHeaders(final String username, String... authorities) {
-		HttpHeaders headers = getNoAuthHeaders();
-		headers.add("Authorization", "Bearer " + addBearerToken(username, authorities));
-		return headers;
-	}
 }
--- src/test/java/gov/usgs/wma/mlrgateway/controller/rt/WorkflowControllerRtTest.java
+++ src/test/java/gov/usgs/wma/mlrgateway/controller/rt/WorkflowControllerRtTest.java
@@ -2,39 +2,49 @@ package gov.usgs.wma.mlrgateway.controller.rt;
 
 import static org.junit.Assert.assertEquals;
 
-import org.junit.Ignore;
 import org.junit.Test;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.util.LinkedMultiValueMap;
 
-@Ignore
 public class WorkflowControllerRtTest extends ControllerRtTest {
 
 	@Test
 	public void postNotAuthenticatedTriesToAuthenticate() {
-		HttpEntity<String> entity = new HttpEntity<String>("", getNoAuthHeaders());
-		ResponseEntity<String> responseEntity = restTemplate.postForEntity("/workflow/ddots", entity, String.class);
+		LinkedMultiValueMap<String, Object> parameters = new LinkedMultiValueMap<>();
+		parameters.add("file", new ClassPathResource("testData/test.ddot"));
+
+		HttpEntity<LinkedMultiValueMap<String, Object>> entity = new HttpEntity<>(parameters, getHeaders(MediaType.MULTIPART_FORM_DATA));
+		ResponseEntity<String> responseEntity = restTemplate.postForEntity("/workflows/ddots", entity, String.class);
 
 		assertEquals(HttpStatus.FOUND, responseEntity.getStatusCode());
-		assertEquals("https://localhost:" + port + "/login", responseEntity.getHeaders().get("Location").get(0));
+		assertEquals("http://localhost:" + port + "/login", responseEntity.getHeaders().get("Location").get(0));
 	}
 
 	@Test
 	public void postNotAuthorizedThrowsUnauthorized() {
-		HttpEntity<String> entity = new HttpEntity<String>("", getAuthHeaders("mlr", "ROLE_ABC"));
-		ResponseEntity<String> responseEntity = restTemplate.postForEntity("/workflow/ddots", entity, String.class);
+		LinkedMultiValueMap<String, Object> parameters = new LinkedMultiValueMap<>();
+		parameters.add("file", new ClassPathResource("testData/test.ddot"));
+
+		HttpEntity<LinkedMultiValueMap<String, Object>> entity = new HttpEntity<>(parameters, getUnauthorizedHeaders(MediaType.MULTIPART_FORM_DATA));
+		ResponseEntity<String> responseEntity = restTemplate.postForEntity("/workflows/ddots", entity, String.class);
 
 		assertEquals(HttpStatus.FORBIDDEN, responseEntity.getStatusCode());
 	}
 
 	@Test
 	public void postValidateNotAuthenticatedTriesToAuthenticate() {
-		HttpEntity<String> entity = new HttpEntity<String>("", getNoAuthHeaders());
-		ResponseEntity<String> responseEntity = restTemplate.postForEntity("/workflow/ddots/validate", entity, String.class);
+		LinkedMultiValueMap<String, Object> parameters = new LinkedMultiValueMap<>();
+		parameters.add("file", new ClassPathResource("testData/test.ddot"));
+
+		HttpEntity<LinkedMultiValueMap<String, Object>> entity = new HttpEntity<>(parameters, getHeaders(MediaType.MULTIPART_FORM_DATA));
+		ResponseEntity<String> responseEntity = restTemplate.postForEntity("/workflows/ddots/validate", entity, String.class);
 
 		assertEquals(HttpStatus.FOUND, responseEntity.getStatusCode());
-		assertEquals("https://localhost:" + port + "/login", responseEntity.getHeaders().get("Location").get(0));
+		assertEquals("http://localhost:" + port + "/login", responseEntity.getHeaders().get("Location").get(0));
 	}
 
 }
