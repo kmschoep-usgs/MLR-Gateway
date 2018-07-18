5cc6e9a22f0649f64504ad2d6848ed668efe6bd0
Kathryn Schoephoester <kmschoep@usgs.gov>
2018-07-18 12:23:11 -0500

    Add test environment to configuration values

--- src/main/java/gov/usgs/wma/mlrgateway/util/ConfigurationValues.java
+++ src/main/java/gov/usgs/wma/mlrgateway/util/ConfigurationValues.java
@@ -6,4 +6,5 @@ public final class ConfigurationValues {
 	public static String ENVIRONMENT_PRODUCTION = "production";
 	public static String ENVIRONMENT_QA = "qa";
 	public static String ENVIRONMENT_DEV = "development";
+	public static String ENVIRONMENT_TEST = "test";
 }
