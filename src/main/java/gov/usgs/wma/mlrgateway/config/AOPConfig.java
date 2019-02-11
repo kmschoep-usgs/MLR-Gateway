fdc7b32d20845f3bf37cab13fc248d356be4cca8
Zack Moore <zmoore@usgs.gov>
2019-02-11 12:44:58 -0600

    Added Spring AOP configuration for profiling transactions when logging level is set to TRACE for org.springframework.aop and gov.usgs.wma.mlrgateway

---
+++ src/main/java/gov/usgs/wma/mlrgateway/config/AOPConfig.java
@@ -0,0 +1,33 @@
+package gov.usgs.wma.mlrgateway.config;
+
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.aop.Advisor;
+import org.springframework.aop.aspectj.AspectJExpressionPointcut;
+import org.springframework.aop.interceptor.PerformanceMonitorInterceptor;
+import org.springframework.aop.support.DefaultPointcutAdvisor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+
+@Configuration
+@EnableAspectJAutoProxy
+@Aspect
+public class AOPConfig {
+    @Pointcut (
+        "execution(public * gov.usgs.wma.mlrgateway.service..*(..))"
+    )
+    public void monitor() {}
+
+    @Bean
+    public PerformanceMonitorInterceptor performanceMonitorInterceptor() {
+        return new PerformanceMonitorInterceptor(false);
+    }
+
+    @Bean
+    public Advisor performanceMonitorAdvistor() {
+        AspectJExpressionPointcut pointcut = new AspectJExpressionPointcut();
+        pointcut.setExpression("gov.usgs.wma.mlrgateway.config.AOPConfig.monitor()");
+        return new DefaultPointcutAdvisor(pointcut, performanceMonitorInterceptor());
+    }
+}
